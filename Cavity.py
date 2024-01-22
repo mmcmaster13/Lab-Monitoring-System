1e1caa9cdefeab980faf02b6a9d159f6998c5381
Marissa McMaster <41704102+mmcmaster13@users.noreply.github.com>
2024-01-22 09:45:48 -0500

    Add files via upload

---
+++ Cavity.py
@@ -0,0 +1,8 @@
+class Cavity:
+
+    def __init__(self, wavelength, address, threshold, center, sample_time):
+        self.wavelength = wavelength
+        self.address = address
+        self.threshold = threshold
+        self.center = center
+        self.sample_time = sample_time
